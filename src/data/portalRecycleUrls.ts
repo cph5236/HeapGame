6171df466de38a432626aa569b7823b31298a2c3
cph5236 <hanlinconnor@gmail.com>
2026-04-21 13:10:39 -0400

    feat: add portalRecycleUrls with Vite-resolved recycle item URLs

---
+++ src/data/portalRecycleUrls.ts
@@ -0,0 +1,22 @@
+import r00 from '../sprites/Heap_sprites/recycle_items/recycle_items_00.png?url';
+import r01 from '../sprites/Heap_sprites/recycle_items/recycle_items_01.png?url';
+import r02 from '../sprites/Heap_sprites/recycle_items/recycle_items_02.png?url';
+import r03 from '../sprites/Heap_sprites/recycle_items/recycle_items_03.png?url';
+import r04 from '../sprites/Heap_sprites/recycle_items/recycle_items_04.png?url';
+import r05 from '../sprites/Heap_sprites/recycle_items/recycle_items_05.png?url';
+import r06 from '../sprites/Heap_sprites/recycle_items/recycle_items_06.png?url';
+import r07 from '../sprites/Heap_sprites/recycle_items/recycle_items_07.png?url';
+import r08 from '../sprites/Heap_sprites/recycle_items/recycle_items_08.png?url';
+import r09 from '../sprites/Heap_sprites/recycle_items/recycle_items_09.png?url';
+import r10 from '../sprites/Heap_sprites/recycle_items/recycle_items_10.png?url';
+import r11 from '../sprites/Heap_sprites/recycle_items/recycle_items_11.png?url';
+import r12 from '../sprites/Heap_sprites/recycle_items/recycle_items_12.png?url';
+import r13 from '../sprites/Heap_sprites/recycle_items/recycle_items_13.png?url';
+import r14 from '../sprites/Heap_sprites/recycle_items/recycle_items_14.png?url';
+import r15 from '../sprites/Heap_sprites/recycle_items/recycle_items_15.png?url';
+
+/** Vite-resolved URLs for all recycle item sprites, indexed 0–15. */
+export const RECYCLE_ITEM_URLS: string[] = [
+  r00, r01, r02, r03, r04, r05, r06, r07,
+  r08, r09, r10, r11, r12, r13, r14, r15,
+];
